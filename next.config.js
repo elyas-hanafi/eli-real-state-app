fd2dccb680fc67aed1372cbee17bf5543459943d
elyas hanafi <elyashanafi79@gmail.com>
2022-09-07 04:28:06 -0700

    first design/index.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images:{
+//     domains:['bayut-production.s3.eu-central-1.amazonaws.com']
+//   }
+// }
+
+// module.exports = nextConfig
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  images:{
+    domains:['bayut-production.s3.eu-central-1.amazonaws.com']
+},
 }
 
-module.exports = nextConfig
